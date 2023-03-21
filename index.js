82575a2479a5b0fe41ee782c854ed60baa62980a
Rolex Omondi <rolexomondi46@gmail.com>
2023-03-21 12:15:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+return drivers.filter(function (user) { return user.toLowerCase() === name.toLowerCase(); } );
+ }
+ findMatching(drivers,"Bobby")
+
+ function fuzzyMatch(drivers, name){
+    return drivers.filter(function(user){
+        return user.slice(0,2) === name.slice(0,2)
+    })
+ };
+
+ function matchName(driver, name){
+    const drivers = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+      return drivers.filter(function(user){
+        return user.name === name
+      })
+ }
